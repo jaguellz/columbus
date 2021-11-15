2865f955bab297cf900446ed86a2ab2c05e8228c
Jaguellz <jaguellz@outlook.com>
2021-11-16 02:18:23 +0300

    final version

--- logger.php
+++ logger.php
@@ -6,6 +6,7 @@ $file = $_FILES['file'];
 //$dir =  SITE_ROOT.'/tmp/'. basename($_FILES['file']['name']);
 //move_uploaded_file($file['tmp_name'], $dir);
 $content = file_get_contents($file['tmp_name']);
+$content = iconv('CP1251', 'UTF-8', $content);
 $lines = explode("\r\n", $content);
 $data = [];
 foreach( $lines as $key => $line ){
@@ -18,7 +19,7 @@ $d1 = array_merge($d1, array_map(function ($v) {
     }, str_split($v[1]));
     $s = array_filter($s);
     $s = implode(',', $s);
-    return '"'.implode('";"', $v).'";'. ($s ? "Недопустимый(е) символ(ы) \"$s\" в поле Название;" : ';');
+    return '"'.implode('";"', $v).'";'. ($s ? "Недопустимый(е) символ(ы) \"$s\" в поле Название" : '');
 }, $data));
 
 $data = array_map(function ($v) {return preg_match('/[^0-9A-Za-zА-Яа-яЁё\-.]/u', $v[1]) ? null:$v;}, $data);
@@ -31,6 +32,7 @@ $q = "INSERT into `$table` (`code`, `name`) VALUES $d2";
 
 $link->query($q);
 $file = fopen('tmp/log.csv', 'w');
+$d1 = iconv("UTF-8", 'CP1251', $d1);
 fwrite($file, $d1);
 fclose($file);
 $file = 'tmp/log.csv';
