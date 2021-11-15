ab90bea958310a1aef992e0583900514a3ba8e16
Jaguellz <jaguellz@outlook.com>
2021-11-16 01:22:04 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="logger.php" enctype="multipart/form-data" method="post">
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    <input type="file" name="file">
+    <br>
+    <br>
+    <input type="submit">
+</form>
---
+++ logger.php
@@ -0,0 +1,43 @@
+<?php
+include "connection.php";
+define ('SITE_ROOT', realpath(dirname(__FILE__)));
+if (!$_FILES) exit();
+$file = $_FILES['file'];
+//$dir =  SITE_ROOT.'/tmp/'. basename($_FILES['file']['name']);
+//move_uploaded_file($file['tmp_name'], $dir);
+$content = file_get_contents($file['tmp_name']);
+$lines = explode("\r\n", $content);
+$data = [];
+foreach( $lines as $key => $line ){
+    $data[] = str_getcsv( $line, ';' );
+}
+$d1 = ["Код;Название;Error"];
+$d1 = array_merge($d1, array_map(function ($v) {
+    $s = array_map(function ($v){
+        return preg_match('/[^0-9A-Za-zА-Яа-яЁё\-.]/u', $v) ? $v:null;
+    }, str_split($v[1]));
+    $s = array_filter($s);
+    $s = implode(',', $s);
+    return '"'.implode('";"', $v).'";'. ($s ? "Недопустимый(е) символ(ы) \"$s\" в поле Название;" : ';');
+}, $data));
+
+$data = array_map(function ($v) {return preg_match('/[^0-9A-Za-zА-Яа-яЁё\-.]/u', $v[1]) ? null:$v;}, $data);
+$d1 = array_filter($d1);
+$d1 = implode("\r\n", $d1);
+$data = array_filter($data);
+$d2 = array_map(function ($v) {return '(\''.implode('\',\'', $v).'\')';}, $data);
+$d2 = implode(',', $d2);
+$q = "INSERT into `$table` (`code`, `name`) VALUES $d2";
+
+$link->query($q);
+$file = fopen('tmp/log.csv', 'w');
+fwrite($file, $d1);
+fclose($file);
+$file = 'tmp/log.csv';
+header('Content-Description: File Transfer');
+header('Content-Type: application/vnd.ms-excel');
+header('Content-Disposition: attachment; filename='.basename($file));
+header('Content-Transfer-Encoding: binary');
+readfile($file);
+unlink($file);
+exit();
